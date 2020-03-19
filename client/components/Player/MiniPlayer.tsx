466c45f309958ac619df78729a5e70892d78fcef
Chris <chrisjobando@gmail.com>
2020-03-19 18:59:53 -0400

    Finished media player controls for mini player

--- client/components/Player/MiniPlayer.tsx
+++ client/components/Player/MiniPlayer.tsx
@@ -1,13 +1,38 @@
+import React, { useState } from 'react';
+
+// Icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBackward,
+  faPause,
+  faPlay,
+  faForward,
+} from '@fortawesome/free-solid-svg-icons';
+
+// Styling
 import classes from './player.module.scss';
 
-const MiniPlayer = () => (
-  <div className={classes.MiniPlayer}>
-    <div className={classes.AlbumPic} />
-    <div className={classes.SongInfo}>
-      <h1 className={classes.SongName}>Sticky</h1>
-      <h2 className={classes.SongArtist}>Ravyn Lenae</h2>
+const MiniPlayer = () => {
+  const [isPlay, setPlay] = useState(false);
+
+  return (
+    <div className={classes.MiniPlayer}>
+      <div className={classes.AlbumPic} />
+      <div className={classes.SongInfo}>
+        <h1 className={classes.SongName}>Sticky</h1>
+        <h2 className={classes.SongArtist}>Ravyn Lenae</h2>
+      </div>
+      <div className={classes.Controls}>
+        <FontAwesomeIcon icon={faBackward} className={classes.Skip} />
+        {isPlay ? (
+          <FontAwesomeIcon icon={faPlay} className={classes.Play} />
+        ) : (
+          <FontAwesomeIcon icon={faPause} className={classes.Play} />
+        )}
+        <FontAwesomeIcon icon={faForward} className={classes.Skip} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default MiniPlayer;
