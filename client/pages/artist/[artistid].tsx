0e2806b817862cf021d769d9f15fa9a945fb9a25
Chris <chrisjobando@gmail.com>
2020-03-20 14:39:24 -0400

    Work on artist page layout

--- client/pages/artist/[artistid].tsx
+++ client/pages/artist/[artistid].tsx
@@ -1,10 +1,19 @@
 import { useRouter } from 'next/router';
+import classes from './artist.module.scss';
 
 const ArtistPage = () => {
   const router = useRouter();
   const { artistid } = router.query;
 
-  return <div>{artistid}</div>;
+  return (
+    <div className={classes.ArtistPage}>
+      <div className={classes.ArtistHead}>
+        <div className={classes.ArtistPic} />
+        <h1 className={classes.ArtistName}>Frank Sinatra</h1>
+      </div>
+      <h1 className={classes.ArtistHeader}>songs.</h1>
+    </div>
+  );
 };
 
 export default ArtistPage;
